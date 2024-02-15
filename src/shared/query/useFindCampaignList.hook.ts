ea26f1f36881c03b83f87b92e68f0c3ebb4e29f2
choi-sang-geun <tkdrms753@gmail.com>
2024-02-15 23:29:48 +0900

    불필요 파일 제거

--- src/shared/query/useFindCampaignList.hook.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { findCampaignList } from "../service/campaigns";
-
-export function useFindCampaignList(page: number) {
-  return useQuery({
-    queryKey: ["campaigns", page],
-    queryFn: () => findCampaignList(page),
-  });
-}
